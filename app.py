32f0572db9d126fded619c3b024f0380933341ed
Max Gollaher <max.c.gollaher@gmail.com>
2024-12-14 11:08:13 -0700

    ping

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Hello, World!'})
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
